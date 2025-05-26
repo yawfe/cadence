42f98ec946e29819b77b4e85b6c8c5e7482eaa9a
Ilya Ozherelyev <iozherelyev@gmail.com>
2025-05-26 22:39:05 +0200

    [common][ringpopprovider] Fix fx ringpop initialization (#6948)

--- common/peerprovider/ringpopprovider/ringpopfx/ringpopfx.go
+++ common/peerprovider/ringpopprovider/ringpopfx/ringpopfx.go
@@ -38,16 +38,16 @@ var Module = fx.Module("ringpop", fx.Provide(buildRingpopProvider))
 type Params struct {
 	fx.In
 
-	Service       string `name:"service"`
-	Config        config.Config
-	ServiceConfig config.Service
-	Logger        log.Logger
-	RPCFactory    rpc.Factory
-	Lifecycle     fx.Lifecycle
+	ServiceFullName string `name:"service-full-name"`
+	Config          config.Config
+	ServiceConfig   config.Service
+	Logger          log.Logger
+	RPCFactory      rpc.Factory
+	Lifecycle       fx.Lifecycle
 }
 
 func buildRingpopProvider(params Params) (membership.PeerProvider, error) {
-	provider, err := ringpopprovider.New(params.Service, &params.Config.Ringpop, params.RPCFactory.GetTChannel(), membership.PortMap{
+	provider, err := ringpopprovider.New(params.ServiceFullName, &params.Config.Ringpop, params.RPCFactory.GetTChannel(), membership.PortMap{
 		membership.PortGRPC:     params.ServiceConfig.RPC.GRPCPort,
 		membership.PortTchannel: params.ServiceConfig.RPC.Port,
 	}, params.Logger)
